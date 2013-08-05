0230b05e7c49678f4281c22d41afb93e3cc963f8
rrajath <r.rajath@gmail.com>
2013-08-04 21:29:57 -0700

    Initial version of LinkExtractor

---
+++ LinkExtractor.py
@@ -0,0 +1,123 @@
+'''
+Created on Aug 4, 2013
+
+@author: rajath
+'''
+import os
+import getpass
+import re
+import lxml.html
+from xml.sax import saxutils
+
+# TODO: write a function to map facebook user-ids with their usernames
+# and display them while searching for a username
+
+dir_path = ''
+
+# Source file path
+src_file_path = os.getcwd()
+
+# Get username on Linux system
+username = getpass.getuser()
+log_path = '/home/' + username + '/.purple/logs/jabber'
+
+accounts = []
+users = []
+urls_dict = {}
+
+# Extracts URL from a ping
+def extract_url(line):
+    # Identify URL and extract it
+    re_exp = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    refined_url = re.findall(re_exp, line)
+    if len(refined_url) == 0:
+        # throw exception
+        pass
+    return list(set([i.strip('<br/>').strip('</a>') for i in refined_url]))
+
+# Searches for users based on the search term provided in the input
+def search_users(accounts, search_term):
+    global dir_path
+    for i in accounts:
+        dir_path = log_path + '/' + i
+        os.chdir(dir_path)
+        for j in os.listdir(dir_path):
+            if search_term in j:
+                users.append(i + '/' + j)
+    return users
+
+# Lists different accounts for which logs are available
+def list_accounts(log_path):
+    for i in os.listdir(log_path):
+        accounts.append(i)
+    return accounts
+
+def get_links(users):
+    # Search in all accounts for that user. For example, the user may have accounts
+    # in both gmail and facebook. The following code will search in both.
+    for i in users:
+        log_files_path = log_path + '/' + i
+        os.chdir(log_files_path)
+        for filename in os.listdir(log_files_path):
+            date = filename.split('.')[0]
+            f = open(filename)
+            lines = f.readlines()
+            f.close()
+            for line in lines:
+                if 'www' in line:
+                    if not urls_dict.get(date):
+                        url_list = []
+                    else:
+                        url_list = urls_dict[date]
+                    urls_dict[date] = url_list + extract_url(line)
+    return urls_dict
+
+# Get the title of the URL to display it more meaningfully
+def get_url_title(url):
+    try:
+        t = lxml.html.parse(url)
+    except:
+        return url
+    title = t.find(".//title").text
+    return saxutils.escape(title)
+
+# Replace placeholders in HTML stub with actual links and corresponding dates
+def insert_links_in_html(urls):
+    output = ''
+    for k, v in urls.iteritems():
+        date = '<h2>' + k + '</h2>'
+        links = ''
+        for link in v:
+            links += '<ul> <a href="' + link + '">' + get_url_title(link) + '</a></ul>'
+        output += date + links
+    return output
+
+# Generate HTML from stub with links and dates replaced
+def generate_html(urls):
+    global users
+    # Stubbed HTMLs are found in data folder in src directory.
+    # Temporarily 'cd'ing to this directory
+    tmp = os.getcwd()
+    os.chdir(src_file_path)
+    html_stub = open('data/html_stub.html')
+    generated_html = open('data/output_html.html', 'w')
+    for line in html_stub:
+        generated_html.write(line.replace('[INSERT_LINKS_HERE]',insert_links_in_html(urls)))
+    html_stub.close()
+    generated_html.close()
+    os.chdir(tmp)
+    return generated_html
+
+accounts = list_accounts(log_path)
+
+print 'Accounts Found: ', accounts
+
+search_term = raw_input('Search username:')
+
+users = search_users(accounts, search_term)
+
+print 'Users found:', users
+
+urls = get_links(users)
+
+generate_html(urls)
